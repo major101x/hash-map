73dba08cc42c07c1690a8dcd8b28263c4d45d209
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2025-05-11 01:34:02 +0100

    create hash method and define default parameters

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 class HashMap {
-    constructor (loadFactor, capacity) {
-        this.loadFactor = loadFactor;
-        this.capacity = capacity;
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    let primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
 
-    
+    return hashCode;
+  }
 }
+
+const test = new HashMap();
+
+console.log(test);
+
+console.log(test.hash("Hello"));
 
 console.log("Console working!");
