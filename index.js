ad84931b853dd8ded7a0a00a0352acd15e2857bf
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2025-05-11 01:06:05 +0100

    create HashMap class

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+class HashMap {
+    constructor (loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+    }
+
+    
+}
+
 console.log("Console working!");
